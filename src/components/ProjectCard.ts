53a233911baa494332263c41780f324f6f93ff79
jestruvec <jhonnyestruve@gmail.com>
2025-08-06 10:37:22 -0600

    fix(global): fixed some problems

--- src/components/ProjectCard.ts
+++ src/components/ProjectCard.ts
@@ -6,48 +6,31 @@ export class ProjectCard extends HTMLElement {
     const techI18nKey = this.getAttribute("technologies-i18n-key") || "";
     const imgUrl = this.getAttribute("img-url") || "";
     const imgAlt = this.getAttribute("img-alt") || "";
-    const repoUrl = this.getAttribute("repo-url") || "";
-    const liveUrl = this.getAttribute("live-url") || "";
+    const liveUrl = this.getAttribute("live-url") || "#";
 
     this.innerHTML = `
-  <article class="h-full flex flex-col gap-4">
-    <header>
-      <h1 class="text-xl font-semibold mb-4" data-i18n="${titleI18nKey}"></h1>
-      <img
-      src="${imgUrl}"
-      alt="${imgAlt}"
-      data-i18n="${imgI18nAltKey}"
-      class="w-full max-w-xl"
-      />
-    </header>
-
-    <section class="flex flex-col flex-grow justify-between gap-4">
-      <p data-i18n="${descriptionI18nKey}"></p>
-      <p class="text-sm font-medium" data-i18n="${techI18nKey}"></p>
-    </section>
-
-    <section class="flex flex-col">
-      <a
-        href="${repoUrl}"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="cursor-pointer hover:bg-[rgba(0,0,0,0.05)] focus:outline-none focus:ring rounded-lg py-1 px-2 ${
-          !repoUrl && "hidden"
-        }"
-      >
-        ${repoUrl}
-      </a>
       <a
         href="${liveUrl}"
         target="_blank"
         rel="noopener noreferrer"
-        class="cursor-pointer hover:bg-[rgba(0,0,0,0.05)] focus:outline-none focus:ring rounded-lg py-1 px-2"
+        class="block overflow-hidden rounded-lg transition bg-white h-full"
+        aria-label="Proyecto"
       >
-        ${liveUrl}
+        <figure class="h-full">
+          <img
+            src="${imgUrl}"
+            alt="${imgAlt}"
+            data-i18n="${imgI18nAltKey}"
+            class="w-full h-64 object-cover"
+          />
+          <figcaption class="p-4 bg-gray-50 h-full">
+            <h3 class="text-lg font-semibold text-gray-800" data-i18n="${titleI18nKey}"></h3>
+            <p class="text-sm text-gray-600 mt-2" data-i18n="${descriptionI18nKey}"></p>
+            <p class="text-xs text-gray-500 mt-2" data-i18n="${techI18nKey}"></p>
+          </figcaption>
+        </figure>
       </a>
-    </section>
-  </article>
-`;
+    `;
   }
 }
 
